933f4107ce1f88e14f806566a05af762dcfcf13b
Rotti_Kim <andy_1009@naver.com>
2023-05-29 01:14:13 +0900

    fix : color

--- src/GlobalStyle.jsx
+++ src/GlobalStyle.jsx
@@ -11,7 +11,7 @@ ${reset}
     font-size: 16px;
     font-family: "Pretendard", "Arial", sans-serif;
     line-height: 1.5;
-    background-color: #1B1A1A;
+    background-color: #0b0b0b;
     color: white;
   }
 `;
